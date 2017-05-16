e4829c2e7b540c26ab53dadf6eb806475a2a029d
WickenChings <ma5687@pleasantonusd.net>
2017-05-15 21:50:52 -0700

    Update Controller.java
    pretty sure these are the basics

--- Calculator/src/application/Controller.java
+++ Calculator/src/application/Controller.java
@@ -4,4 +4,60 @@ public class Controller {
 	public void digitClicked(){
 		System.out.println(7);
 	}
+	
+	public void add(){
+	}
+	
+	public void subtract(){
+	}
+	
+	public void multiply(){
+	}
+	
+	public void divide(){
+	}
+	
+	public void clear(){
+	}
+	
+	public void sin(){
+	}
+	
+	public void cos(){
+	}
+	
+	public void tan(){
+	}
+	
+	public void log(a, b){
+	}
+	
+	public void power(a, b){
+	}
+	
+	public void root(a, b){
+	}
+	
+	public void factorial(a){
+	}
+	
+	public void ln(a, b){
+	}
+	
+	//probably need a switcher between radians and degrees
+	
+	public void calculateFinalGrade(double finalPercent, double currentGrade){
+	}
+	
+	public void delete(){
+	}
+	
+	public void enter(){
+	}
+	
+	public void scientificNotation(){
+	}
+	
+	
+	
 }
